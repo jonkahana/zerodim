5e60cd7fe0f65a4384726b39a78da6134b721db6
jonkahana <jonkahana1996@gmail.com>
2022-01-23 16:03:03 +0200

    add bash scripts to automatically train all 3 datasets

---
+++ bash_scripts/smallnorb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /cs/labs/yedid/jonkahana/lord_cl_venv/bin/activate.csh
+module load tensorflow/2.5.0
+export CUDA_HOME="/usr/local/nvidia/cuda/11.0/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64"
+
+python -u main.py train --config smallnorb --data-name smallnorb-x64 --model-name zerodim-smallnorb-x64
