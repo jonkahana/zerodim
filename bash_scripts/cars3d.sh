48c752075a6eba16af6560118838ba14ac0ec0e8
jonkahana <jonkahana1996@gmail.com>
2022-01-25 21:13:26 +0200

    debug attempt to remove residual code

--- bash_scripts/cars3d.sh
+++ bash_scripts/cars3d.sh
@@ -2,6 +2,8 @@
 
 source /cs/labs/yedid/jonkahana/lord_cl_venv/bin/activate.csh
 module load tensorflow/2.5.0
+module load cuda/11.0
+
 export CUDA_HOME="/usr/local/nvidia/cuda/11.0/"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64"
 
