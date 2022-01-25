d75c068a5487c88f186eac22feae50148e486e6a
jonkahana <jonkahana1996@gmail.com>
2022-01-25 20:58:54 +0200

    debug attempt to remove residual code

--- bash_scripts/sbatch_all.sh
+++ bash_scripts/sbatch_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sbatch --mem=24g -c4 --time=4-0 --gres=gpu:1,vmem:10g --output=logfiles/cars3d__zerodim_training.log --killable --job-name=0dim_cars bash_scripts/dsprites.sh
+sbatch --mem=24g -c4 --time=4-0 --gres=gpu:1,vmem:20g --output=logfiles/cars3d__zerodim_training.log --killable --job-name=0dim_cars bash_scripts/dsprites.sh
 
 #sbatch --mem=24g -c4 --time=4-0 --gres=gpu:1,vmem:10g --output=logfiles/dsprites__zerodim_training.log --job-name=0dim_dsprites bash_scripts/cars3d.sh
 
